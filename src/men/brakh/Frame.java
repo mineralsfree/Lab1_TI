85be2bf1b6ce9fbfb7fc76460e150b31e870471a
Nikita Pilinko <mineralsfree@gmail.com>
2018-09-19 21:35:21 +0300

    12
    21

--- src/men/brakh/Frame.java
+++ src/men/brakh/Frame.java
@@ -10,7 +10,9 @@ public class Frame extends JFrame {
     static String onlyEng(String s) {
         return s.replaceAll("[^A-Za-z�]", "").toUpperCase();
     }
-
+	static String onlyRus(String s) {
+		return s.replaceAll("[^А-Яа-я�]", "").toUpperCase();
+	}
 
 	public Frame() {
 		super("Enter Key and Text to shifr");
@@ -45,8 +47,7 @@ public static class Field extends JTextField{
 }
 	public static void main(String[] args) throws IOException {
 		Pleifer pf = new Pleifer();
-		Viginer vg = new Viginer();
-		RailWay rw = new RailWay();
+
 
 		JRadioButton english = new JRadioButton("english");
 		JRadioButton russian = new JRadioButton("russian");
@@ -65,45 +66,81 @@ public static class Field extends JTextField{
 		operation.add(decode);
 		lang.add(english);
 		lang.add(russian);
+		encode.setSelected(true);
+		english.setSelected(true);
+		Pleifer.setSelected(true);
 		File input = new File("C:\\Users\\User\\Documents\\GitHub\\Lab1_TI\\src\\men\\brakh\\Input.txt");
 		File output= new File("C:\\Users\\User\\Documents\\GitHub\\Lab1_TI\\src\\men\\brakh\\Output.txt");
 		File key = new File("C:\\Users\\User\\Documents\\GitHub\\Lab1_TI\\src\\men\\brakh\\key.txt");
 		String inputString = Field.readFile2(input);
 		String keyString = Field.readFile2(key);
-		FileWriter fw = new FileWriter(output);
-		if (inputString!=null){
-			try {
-				fw.write(pf.encode(inputString,""));
-				fw.flush();
-			}catch (IOException e){
-
-			}
-		}
-		fw.close();
 		Frame app = new Frame();
 		JPanel panel = new JPanel(new  GridLayout(6,3));
 		app.add(panel);
-		//app
 		app.getContentPane().setBackground(Color.darkGray);
-
 		 JButton encodeButton = new JButton();
-		 JTextArea ta2 = new JTextArea();
 		encodeButton.setText("Perfom Action");
 		
 		encodeButton.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
 			String message = inputString;
-			message = onlyEng(message);
+				String OutputString = "";
+			if (russian.isSelected()){
+				message=Frame.onlyRus(message);
+
+			} else{
+				message=Frame.onlyEng(message);
+			}
 			String key = keyString;
-			message.toUpperCase();
-			
-			String encodedMessage = rw.encode(message, key);
+			if (encode.isSelected()){
+				if (Viginer.isSelected()){
+					Viginer vg = new Viginer();
+					 OutputString = vg.encode(message,key);
+				}
+				if (Pleifer.isSelected()){
+					Pleifer pf = new Pleifer();
+					 OutputString = pf.encode(message,key);
+				}
+				if (Railway.isSelected()){
+					RailWay rw = new RailWay();
+					 OutputString = rw.encode(message,key);
+				}
+			}	else{
+				if (Viginer.isSelected()){
+					Viginer vg = new Viginer();
+					 OutputString = vg.decode(message,key);
+				}
+				if (Pleifer.isSelected()){
+					Pleifer pf = new Pleifer();
+					 OutputString = pf.decode(message,key);
+				}
+				if (Railway.isSelected()){
+					RailWay rw = new RailWay();
+					 OutputString = rw.decode(message,key);
+				}
+			}
+				FileWriter fw = null;
+				try {
+					fw = new FileWriter(output);
+				} catch (IOException e1) {
+					e1.printStackTrace();
+				}
+				try {
+				if (inputString!=null){
+
+						fw.write(OutputString);
+						fw.flush();
+					}
+
+				fw.close();}catch (IOException l){
+
+				}
+
 
 			}
 		}
             );
-
 
 		panel.add(decode);
 		panel.add(encode);
@@ -113,9 +150,6 @@ public static class Field extends JTextField{
 		panel.add(Railway);
 		panel.add(Viginer);
 		panel.add(encodeButton);
-
-		
-		
 		app.setVisible(true);
 	}
 
